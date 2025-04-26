e4b5e28db1d921d102b4cae27cfdb5368d175ee6
Roman Zheglov <damdark107@gmail.com>
2025-04-27 02:54:41 +0700

    Remove Hashable and Equatable conformances and related implementations from extensions
    Bad architectural decision. Cleaned up conformances and extensions.

--- InfoSecNews/Model/NewsBehavior.swift
+++ InfoSecNews/Model/NewsBehavior.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-protocol NewsBehavior: Hashable, Identifiable, Equatable, AnyObject {
+protocol NewsBehavior: Identifiable, AnyObject {
     var source: String { get }
     var title: String { get }
     var date: Date { get }
@@ -16,24 +16,4 @@ protocol NewsBehavior: Hashable, Identifiable, Equatable, AnyObject {
     var full: String? { get }
 
     func loadRemoteData(voyager: WebVoyager) async throws
-}
-
-extension NewsBehavior  {
-    func hash(into hasher: inout Hasher) {
-         hasher.combine(source)
-         hasher.combine(title)
-         hasher.combine(date)
-         hasher.combine(short)
-         hasher.combine(fullTextLink)
-         hasher.combine(full)
-     }
-
-    static func == (lhs: Self, rhs: Self) -> Bool {
-         return lhs.source == rhs.source &&
-                lhs.title == rhs.title &&
-                lhs.date == rhs.date &&
-                lhs.short == rhs.short &&
-                lhs.fullTextLink == rhs.fullTextLink &&
-                lhs.full == rhs.full
-     }
 }
