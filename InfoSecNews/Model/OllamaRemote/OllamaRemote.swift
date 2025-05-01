9af1be6b7bfa69041e11df0a4aca1677d01c38ca
Roman Zheglov <damdark107@gmail.com>
2025-05-01 23:29:43 +0700

    Rewrite readRemoteStream. readRemoteStream is now readRemoteStreamBytes.

--- InfoSecNews/Model/OllamaRemote/OllamaRemote.swift
+++ InfoSecNews/Model/OllamaRemote/OllamaRemote.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import os
 
 class OllamaRemote {
     let remoteServerURL: URL = URL(string: "http://127.0.0.1:11434")!
@@ -15,10 +16,14 @@ class OllamaRemote {
         self.selectedModel = selectedModel
     }
     
-    func generateStream(prompt: String, system: String? = nil, onDataAppeared: @escaping (String) -> Void) {
+    func generateStream(
+        prompt: String,
+        system: String? = nil,
+        onDataAppeared: @escaping (String) -> Void
+    ) {
         let generationURL = remoteServerURL.appending(path: "/api/generate")
         
-        var requestBody: [String: Any] = [
+        var requestBody: [String: String] = [
             "model": selectedModel.name,
             "prompt": prompt
         ]
@@ -28,13 +33,17 @@ class OllamaRemote {
         }
         
         Task {
-            try! await readRemoteStream(
-                onURL: generationURL,
-                body: requestBody,
-                onJSONAppeared: { (response: OllamaStreamResponse) in
-                    onDataAppeared(response.response)
+            let array = try! await readRemoteStream(streamURL: generationURL, jsonBody: requestBody, as: OllamaStreamResponse.self)
+            
+            do {
+                
+                for try await item in array {
+                    onDataAppeared(item.response)
                 }
-            )
+            } catch {
+                Logger.ollamaLogger.error("\(error.localizedDescription)")
+            }
+            
         }
     }
     
@@ -57,42 +66,64 @@ class OllamaRemote {
     }
     
     private func readRemoteStream(
-        onURL: URL,
-        body: [String: Any],
-        onLineAppeared: @escaping (String) -> Void
-    ) async throws {
-        var request = URLRequest(url: onURL)
+        streamURL: URL,
+        jsonBody: any Encodable
+    ) async throws -> URLSession.AsyncBytes {
+        var request = URLRequest(url: streamURL)
         request.httpMethod = "POST"
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         
-        request.httpBody = try JSONSerialization.data(withJSONObject: body)
+        request.httpBody = try JSONEncoder().encode(jsonBody)
         
         let (stream, _) = try await URLSession.shared.bytes(for: request)
         
-        for try await byte in stream.lines {
-            onLineAppeared(byte)
-        }
+        return stream
     }
     
     private func readRemoteStream<T: Decodable>(
-        onURL: URL,
-        body: [String: Any],
-        onJSONAppeared: @escaping (T) -> Void
-    ) async throws {
-        var request = URLRequest(url: onURL)
-        request.httpMethod = "POST"
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        streamURL: URL,
+        jsonBody: any Encodable,
+        as: T.Type
+    ) async throws -> NDJsonStream<T> {
+        let byteStream = try await readRemoteStream(streamURL: streamURL, jsonBody: jsonBody)
+        let ndjsonStream = NDJsonStream<T>(bytes: byteStream)
+        return ndjsonStream
+    }
+}
+
+struct NDJsonStream<T: Decodable>: AsyncSequence {
+    typealias Element = T
+    typealias AsyncIterator = NDJsonIterator
+    
+    let bytes: URLSession.AsyncBytes
+    
+    struct NDJsonIterator: AsyncIteratorProtocol {
+        var iterator: URLSession.AsyncBytes.Iterator
+        var buffer = Data()
         
-        request.httpBody = try JSONSerialization.data(withJSONObject: body)
-        
-        let (stream, _) = try await URLSession.shared.bytes(for: request)
-        
-        for try await byte in stream.lines {
-            guard let data = byte.data(using: .utf8) else { continue }
-            let decoder = JSONDecoder()
-            decoder.keyDecodingStrategy = .convertFromSnakeCase
-            let JSONObject = try decoder.decode(T.self, from: data)
-            onJSONAppeared(JSONObject)
+        mutating func next() async throws -> Element? {
+            while true {
+                // FYI: data == 0x0A is newline
+                if let jsonStrSubrange = buffer.firstRange(of: Data([0x0A])) {
+                    let line = buffer.prefix(upTo: jsonStrSubrange.lowerBound)
+                    buffer.removeSubrange(...jsonStrSubrange.lowerBound)
+                    let decoder = JSONDecoder()
+                    
+                    decoder.keyDecodingStrategy = .convertFromSnakeCase
+                    
+                    return try decoder.decode(T.self, from: line)
+                }
+                
+                guard let next = try await iterator.next() else {
+                    return nil
+                }
+                
+                buffer.append(next)
+            }
         }
+    }
+    
+    func makeAsyncIterator() -> NDJsonIterator {
+        return NDJsonIterator(iterator: bytes.makeAsyncIterator())
     }
 }
