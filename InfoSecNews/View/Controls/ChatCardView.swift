e101943ff078278f3445be41ec5757a04acf054d
Roman Zheglov <damdark107@gmail.com>
2025-05-14 03:59:18 +0700

    Refactor deeply nested views as part of issue #30 for NewsConversationView

--- InfoSecNews/View/Controls/ChatCardView.swift
+++ InfoSecNews/View/Controls/ChatCardView.swift
@@ -21,119 +21,133 @@ struct ChatCardView: View {
     // TODO: Disable autoscroll
     // TODO: ZStack instead of VStack. Shrink padding and enable background blur
     
-    var body: some View {
-        VStack {
-            ScrollViewReader { proxy in
-                ScrollView {
-                    VStack {
-                        HStack {
-                            HStack(spacing: 0) {
-                                Button(action: { isOrignalPresented = false }) {
-                                    Image(systemName: "quote.bubble")
-                                        .padding(.vertical, 8)
-                                        .padding(.leading, 8)
-                                        .padding(.horizontal, 8)
-                                        .fontWeight(.semibold)
-                                        .foregroundStyle(isOrignalPresented ? .gray : .blue)
-                                        .contentShape(Rectangle())
-                                }.buttonStyle(.plain)
-                                Button(action: { isOrignalPresented = true }) {
-                                    Image(systemName: "newspaper")
-                                        .padding(.vertical, 8)
-                                        .padding(.horizontal, 8)
-                                        .padding(.trailing, 8)
-                                        .fontWeight(.semibold)
-                                        .foregroundStyle(isOrignalPresented ? .blue : .gray)
-                                        .contentShape(Rectangle())
-                                }.buttonStyle(.plain)
-                            }
-                            .background(.gray.opacity(0.1))
-                            .clipShape(Capsule())
-                            Spacer()
-                            if let action = closeAction {
-                                Button(action: {
-                                    action()
-                                }) {
-                                    Image(systemName: "xmark")
-                                        .padding(.vertical, 8)
-                                        .padding(.horizontal, 8)
-                                        .fontWeight(.semibold)
-                                        .background(.gray.opacity(0.1))
-                                        .foregroundStyle(.secondary)
-                                        .clipShape(Circle())
-                                }.buttonStyle(.plain)
-                            }
-                        }
-                        ChatResponse(conversation: conversation,
-                                     isOriginal: $isOrignalPresented)
-                        .matchedGeometryEffect(id: conversation.id, in: parentNameSpace)
-                    }
-                    .padding(.top, 16)
-                    .padding(.horizontal, 16)
-                    Text("Conversation started")
-                        .font(.callout)
+    var responseHeader: some View {
+        HStack {
+            HStack(spacing: 0) {
+                Button(action: { isOrignalPresented = false }) {
+                    Image(systemName: "quote.bubble")
+                        .padding(.vertical, 8)
+                        .padding(.leading, 8)
+                        .padding(.horizontal, 8)
                         .fontWeight(.semibold)
-                        .foregroundStyle(.secondary)
-                        .padding(16)
-                    VStack(alignment: .leading, spacing: 16) {
-                        ForEach(conversation.storage.filter({ $0.role != .system })) { item in
-                            HStack {
-                                if item.role == .user {
-                                    Spacer()
-                                }
-                                if item.role == .user {
-                                    Text(item.content)
-                                        .padding(.vertical, 8)
-                                        .padding(.horizontal, 16)
-                                        .foregroundStyle(.white)
-                                        .background(.blue)
-                                        .clipShape(MessageBubble(isUserMessage: true))
-                                        .padding(.leading, 128)
-                                        .textSelection(.enabled)
-                                } else {
-                                    Text(item.content)
-                                        .padding(.vertical, 8)
-                                        .padding(.horizontal, 16)
-                                        .background(.gray.opacity(0.1))
-                                        .clipShape(MessageBubble(isUserMessage: false))
-                                        .padding(.trailing, 128)
-                                        .textSelection(.enabled)
-                                        .onChange(of: item.content) {
-                                            proxy.scrollTo("CHAT_BOTTOM", anchor: .bottom)
-                                        }
-                                }
-                            }
-                        }
-                        
-                        Color.clear.id("CHAT_BOTTOM")
-                    }.padding(.horizontal, 16)
-                    
+                        .foregroundStyle(isOrignalPresented ? .gray : .blue)
+                        .contentShape(Rectangle())
+                }.buttonStyle(.plain)
+                Button(action: { isOrignalPresented = true }) {
+                    Image(systemName: "newspaper")
+                        .padding(.vertical, 8)
+                        .padding(.horizontal, 8)
+                        .padding(.trailing, 8)
+                        .fontWeight(.semibold)
+                        .foregroundStyle(isOrignalPresented ? .blue : .gray)
+                        .contentShape(Rectangle())
+                }.buttonStyle(.plain)
+            }
+            .background(.gray.opacity(0.1))
+            .clipShape(Capsule())
+            Spacer()
+            HStack {
+                Spacer()
+                if let action = closeAction {
+                    Button(action: {
+                        action()
+                    }) {
+                        Image(systemName: "xmark")
+                            .padding(.vertical, 8)
+                            .padding(.horizontal, 8)
+                            .fontWeight(.semibold)
+                            .background(.gray.opacity(0.1))
+                            .foregroundStyle(.secondary)
+                            .clipShape(Circle())
+                    }.buttonStyle(.plain)
                 }
             }
-            HStack(spacing: 8) {
-                TextField("Message", text: $message, axis: .vertical)
-                    .lineLimit(5)
-                    .padding(.vertical, 6)
-                    .padding(.horizontal, 12)
-                    .overlay {
-                        RoundedRectangle(cornerRadius: 24)
-                            .stroke(.gray.opacity(0.4), lineWidth: 1)
+        }
+    }
+    
+    func makeChatBody(scrollProxy proxy: ScrollViewProxy) -> some View {
+        VStack(alignment: .leading, spacing: 16) {
+            ForEach(conversation.storage.filter({ $0.role != .system })) { item in
+                HStack {
+                    if item.role == .user {
+                        Spacer()
                     }
-                    .contentShape(Capsule(style: .continuous))
-                    .focused($isFieldFocused)
-                    .textFieldStyle(.plain)
-                    .onTapGesture {
-                        isFieldFocused = !isFieldFocused
+                    if item.role == .user {
+                        Text(item.content)
+                            .padding(.vertical, 8)
+                            .padding(.horizontal, 16)
+                            .foregroundStyle(.white)
+                            .background(.blue)
+                            .clipShape(MessageBubble(isUserMessage: true))
+                            .padding(.leading, 128)
+                            .textSelection(.enabled)
+                    } else {
+                        Text(item.content)
+                            .padding(.vertical, 8)
+                            .padding(.horizontal, 16)
+                            .background(.gray.opacity(0.1))
+                            .clipShape(MessageBubble(isUserMessage: false))
+                            .padding(.trailing, 128)
+                            .textSelection(.enabled)
+                            .onChange(of: item.content) {
+                                proxy.scrollTo("CHAT_BOTTOM", anchor: .bottom)
+                            }
                     }
-                    .onSubmit(sendMessage)
-                Button(action: sendMessage) {
-                    Image(systemName: "paperplane")
-                        .fontWeight(.semibold)
-                        .font(.title3)
-                        .contentShape(Rectangle())
-                        .foregroundStyle(.secondary)
-                }.buttonStyle(.plain)
-            }.padding(16)
+                }
+            }
+            
+            Color.clear.id("CHAT_BOTTOM")
+        }
+    }
+    
+    var chatField: some View {
+        HStack(spacing: 8) {
+            TextField("Message", text: $message, axis: .vertical)
+                .lineLimit(5)
+                .padding(.vertical, 6)
+                .padding(.horizontal, 12)
+                .overlay {
+                    RoundedRectangle(cornerRadius: 24)
+                        .stroke(.gray.opacity(0.4), lineWidth: 1)
+                }
+                .contentShape(Capsule(style: .continuous))
+                .focused($isFieldFocused)
+                .textFieldStyle(.plain)
+                .onTapGesture {
+                    isFieldFocused = !isFieldFocused
+                }
+                .onSubmit(sendMessage)
+            Button(action: sendMessage) {
+                Image(systemName: "paperplane")
+                    .fontWeight(.semibold)
+                    .font(.title3)
+                    .contentShape(Rectangle())
+                    .foregroundStyle(.secondary)
+            }.buttonStyle(.plain)
+        }
+    }
+    
+    var body: some View {
+        ScrollViewReader { proxy in
+            ScrollView {
+                VStack {
+                    responseHeader
+                    ChatResponse(conversation: conversation,
+                                 isOriginal: $isOrignalPresented)
+                    .matchedGeometryEffect(id: conversation.id, in: parentNameSpace)
+                }
+                .padding(.top, 16)
+                .padding(.horizontal, 16)
+                Text("Conversation started")
+                    .font(.callout)
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.secondary)
+                    .padding(16)
+                makeChatBody(scrollProxy: proxy)
+                    .padding(.horizontal, 16)
+                chatField
+                    .padding(16)
+            }
         }
     }
     
